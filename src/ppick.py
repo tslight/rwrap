d5045d76f08c7b35067ab84d418e3273d636a276
tslight <tobyslight@gmail.com>
2018-07-08 21:33:59 +0100

    heavy refactoring (unpicking ppick!)

--- src/ppick.py
+++ src/ppick.py
@@ -47,67 +47,70 @@ def listdir_nohidden(path):
             yield f
 
 
-class File:
+# class File:
+#     def __init__(self, name, hidden):
+#         self.name = name
+#         self.hidden = hidden
+#         self.marked = False
+#         self.expanded = False
+
+#     def icon1(self): return '   '
+
+#     def icon2(self):
+#         if self.marked:
+#             return ' * '
+#         else:
+#             return ''
+
+#     def traverse(self): yield self, 0
+
+#     def expand(self): pass
+
+#     def collapse(self): pass
+
+#     def mark(self): self.marked = True
+
+#     def unmark(self): self.marked = False
+
+
+class Dir:
     def __init__(self, name, hidden):
+        # if not os.path.isdir(name):
+        #     File(name, hidden)
+
         self.name = name
         self.hidden = hidden
-        self.marked = False
+
+        try:
+            if self.hidden:
+                self.children = sorted(listdir_nohidden(name))
+            else:
+                self.children = sorted(os.listdir(name))
+        except:
+            self.children = None  # probably permission denied
+
+        self.childpaths = None
         self.expanded = False
+        self.marked = False
 
     def render(self, depth, width):
         return pad('%s%s %s%s' % (' ' * 4 * depth, self.icon1(),
                                   os.path.basename(self.name), self.icon2()), width)
 
-    def icon1(self): return '   '
-
-    def icon2(self):
-        if self.marked:
-            return ' * '
-        else:
-            return ''
-
-    def traverse(self): yield self, 0
-
-    def expand(self): pass
-
-    def collapse(self): pass
-
-    def mark(self): self.marked = True
-
-    def unmark(self): self.marked = False
-
-
-class Dir(File):
-    def __init__(self, name, hidden):
-        File.__init__(self, name, hidden)
-        self.hidden = hidden
-        try:
-            if self.hidden:
-                self.kidnames = sorted(listdir_nohidden(name))
-            else:
-                self.kidnames = sorted(os.listdir(name))
-        except:
-            self.kidnames = None  # probably permission denied
-        self.kids = None
-        self.expanded = False
-        self.marked = False
-
-    def children(self):
-        if self.kidnames is None:
-            return []
-        if self.kids is None:
-            # for kid in self.kidnames:
-            #     self.kids = mknode(os.path.join(self.name, kid), self.hidden)
-            self.kids = [mknode(os.path.join(self.name, kid), self.hidden)
-                         for kid in self.kidnames]
-        return self.kids
+    def get_children(self):
+        if self.children is None:
+            return
+        if self.childpaths is None:
+            self.childpaths = [Dir(os.path.join(self.name, child), self.hidden)
+                               for child in self.children]
+        return self.childpaths
 
     def icon1(self):
         if self.expanded:
             return '[-]'
-        elif self.children():
+        elif self.get_children():
             return '[+]'
-        elif self.kidnames is None:
+        elif self.children is None:
             return '[?]'
         else:
             return '[ ]'
@@ -127,29 +130,29 @@ class Dir(File):
     def unmark(self): self.marked = False
 
     def traverse(self):
-        yield (self, 0)
+        yield self, 0
         if not self.expanded:
             return
 
-        for child in self.children():
-            for kid, depth in child.traverse():
-                yield kid, depth + 1
+        for child in self.get_children():
+            for c, depth in child.traverse():
+                yield c, depth + 1
 
 
-def parse_keys(ch, curidx):
+def parse_keys(ch, curline):
     action = None
     if ch == curses.KEY_UP or ch == ord('k') or ch == ord('p'):
-        curidx -= 1
+        curline -= 1
     elif ch == curses.KEY_DOWN or ch == ord('j') or ch == ord('n'):
-        curidx += 1
+        curline += 1
     elif ch == curses.KEY_RIGHT or ch == ord('l') or ch == ord('f'):
         action = 'expand'
     elif ch == curses.KEY_LEFT or ch == ord('h') or ch == ord('b'):
         action = 'collapse'
     elif ch == curses.KEY_HOME or ch == ord('g') or ch == ord('<'):
-        curidx = 0
+        curline = 0
     elif ch == curses.KEY_END or ch == ord('G') or ch == ord('>'):
-        curidx = line - 1
+        curline = line - 1
     elif ch == ord('\t') or ch == ord('\n'):
         action = 'toggle_expand'
     elif ch == ord('m') or ch == ord(' '):
@@ -158,20 +161,13 @@ def parse_keys(ch, curidx):
         action = 'toggle_hidden'
     elif ch == ord('r'):
         action = 'reset'
-    return (action, curidx)
-
-
-def mknode(path, hidden):
-    if os.path.isdir(path):
-        return Dir(path, hidden)
-    else:
-        return File(path, hidden)
+    return (action, curline)
 
 
 def select(stdscr, root, hidden):
-    node = mknode(root, hidden)
-    node.expand()
-    curidx = 1
+    parent = Dir(root, hidden)
+    parent.expand()
+    curline = 1
     action = None
     ESC = 27
     selected = []
@@ -180,47 +176,48 @@ def select(stdscr, root, hidden):
         stdscr.erase()  # https://stackoverflow.com/a/24966639
         curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
         line = 0
-        offset = max(0, curidx - curses.LINES + 10)
+        offset = max(0, curline - curses.LINES + 10)
 
-        for data, depth in node.traverse():
-            if line == curidx:
+        children = parent.traverse()
+
+        for child, depth in children:
+            if line == curline:
                 stdscr.attrset(curses.color_pair(1) | curses.A_BOLD)
                 if action == 'toggle_mark':
-                    if data.marked:
-                        data.unmark()
-                        selected.remove(data.name)
+                    if child.marked:
+                        child.unmark()
+                        selected.remove(child.name)
                     else:
-                        data.mark()
-                        selected.append(data.name)
+                        child.mark()
+                        selected.append(child.name)
                     stdscr.attrset(curses.color_pair(0))
-                    curidx += 1
+                    curline += 1
                 elif action == 'toggle_expand':
-                    if data.expanded:
-                        data.collapse()
+                    if child.expanded:
+                        child.collapse()
                     else:
-                        data.expand()
+                        child.expand()
                 elif action == 'toggle_hidden':
-                    # such a terrible hack!
                     if hidden:
                         hidden = False
-                        selected = select(stdscr, root, hidden)
-                        return selected
+                        parent = Dir(root, hidden)
+                        parent.expand()
                     else:
                         hidden = True
-                        selected = select(stdscr, root, hidden)
-                        return selected
+                        parent = Dir(root, hidden)
+                        parent.expand()
                 elif action == 'reset':
-                    # such a terrible hack!
-                    selected = select(stdscr, root)
-                    return selected
+                    parent = Dir(root, hidden)
+                    parent.expand()
+                    selected = []
                 elif action:
-                    getattr(data, action)()
+                    getattr(child, action)()
                 action = None
             else:
                 stdscr.attrset(curses.color_pair(0))
             if 0 <= line - offset < curses.LINES - 1:
                 stdscr.addstr(line - offset, 0,
-                              data.render(depth, curses.COLS))
+                              child.render(depth, curses.COLS))
             line += 1
 
         stdscr.refresh()
@@ -228,10 +225,10 @@ def select(stdscr, root, hidden):
         if ch == ord('e') or ch == ord('q') or ch == ESC:
             return selected
         else:
-            keyresult = parse_keys(ch, curidx)
+            keyresult = parse_keys(ch, curline)
             action = keyresult[0]
-            curidx = keyresult[1]
-        curidx %= line
+            curline = keyresult[1]
+        curline %= line
 
 
 def open_tty():
@@ -259,9 +256,10 @@ def get_args():
     """
     parser = argparse.ArgumentParser(description='\
     Select paths from a directory tree.')
-    parser.add_argument("-a", "--hidden", action="store_true",
+    parser.add_argument("-a", "--hidden", action="store_false",
                         help="Show all hidden paths too.")
-    parser.add_argument("path", type=str, default='.', help="A valid path.")
+    parser.add_argument("path", type=str, nargs='?',
+                        default=".", help="A valid path.")
     return parser.parse_args()
 
 
@@ -275,4 +273,4 @@ if __name__ == '__main__':
     finally:
         restore_stdio(saved_fds)
 
-    print(paths)
+    print("\n".join(paths))
