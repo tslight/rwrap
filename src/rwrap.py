39ea05741251f23675ee6ebbcfbabd06da5b232f
tslight <tobyslight@gmail.com>
2018-07-11 11:47:18 +0100

    added hidden parameter to ppick select function calls

--- src/rwrap.py
+++ src/rwrap.py
@@ -35,21 +35,21 @@ def check_dir(path):
             print("Could not create directory.")
 
 
-def get_excludes(path):
+def get_excludes(path, hidden):
     """
     Takes a path as an argument and returns a list of child paths that the user
     has selected.
     """
-    excludes = curses.wrapper(select, path)
+    excludes = curses.wrapper(select, path, hidden)
     if len(excludes) > 0:
         print("\nSelected excludes:\n"+('\n'.join(excludes)))
         if ask("\nAccept and use excludes? "):
             return excludes
         else:
-            excludes = get_excludes(path)
+            excludes = get_excludes(path, hidden)
     else:
         if not(ask("\nNo excludes selected. Continue? ")):
-            excludes = get_excludes(path)
+            excludes = get_excludes(path, hidden)
     return excludes
 
 
@@ -102,12 +102,13 @@ def copy_skel(date, user, url):
 
 
 def rwrap(src, dest, automate_excludes):
+    hidden = True
     if automate_excludes:
         cmd = "rsync " + " ".join(RSYNC_OPTS) + \
             " ".join(DEFAULT_EXCLUDES) + " " + src + " " + dest
         subprocess.call(cmd, shell=True)
     else:
-        excludes = get_excludes(src)
+        excludes = get_excludes(src, hidden)
         # list + set removes duplicates.
         excludes = excludes + list(set(excludes + DEFAULT_EXCLUDES))
         cmd = "rsync " + " ".join(RSYNC_OPTS) + \
